c2319ce01f4ba46e60747fa2049c72112831dd66
Bernyakovich Nikolay <nikolay.bernyakovich@yandex.ru>
2020-11-23 15:18:52 +0300

    Fixed mistakes

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-
+		String answer;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter first integer number:");
 		calculator.setNum1(scan.nextInt());
@@ -13,5 +13,21 @@ public class CalculatorTest {
 		calculator.setNum2(scan.nextInt());
 		float result = calculator.calculate();
 		System.out.println("Result = " + result);
+		while (true) {
+			System.out.println("Continue (y/n)?");
+			answer = scan.next();
+			if (answer.contentEquals("y")) {
+				System.out.println("Enter first integer number:");
+				calculator.setNum1(scan.nextInt());
+				System.out.println("Enter sign of operation (+, -, *, /, %, ^):");
+				calculator.setOperation(scan.next().charAt(0));
+				System.out.println("Enter second integer number:");
+				calculator.setNum2(scan.nextInt());
+				result = calculator.calculate();
+				System.out.println("Result = " + result);
+			} else if (answer.contentEquals("n")) {
+				break;
+			} 
+		}
 	}
 }
