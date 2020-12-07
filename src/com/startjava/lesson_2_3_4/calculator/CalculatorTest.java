376b945a8355fc4f333a9caf81fe0b253ed1e1ec
Nikolay Bernyakovich <n.bernyakovich@gmail.com>
2020-12-07 21:30:31 +0300

    Rename lesson directory

---
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.startjava.lesson_2_3_4.calculator;
+
+import java.util.*;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        String answer;
+        Scanner scan = new Scanner(System.in);
+        answer = "y";
+        while (!answer.contentEquals("n")) {
+            if (answer.contentEquals("y")) {
+                System.out.println("Enter first integer number:");
+                calculator.setNum1(scan.nextInt());
+                System.out.println("Enter sign of operation (+, -, *, /, %, ^):");
+                calculator.setOperation(scan.next().charAt(0));
+                System.out.println("Enter second integer number:");
+                calculator.setNum2(scan.nextInt());
+                System.out.println("Result = " + calculator.calculate());
+            }
+            System.out.println("Continue (y/n)?");
+            answer = scan.next();
+        }
+    }
+}
